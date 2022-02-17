fe2422721a862b62aa30fadca93f71a3c0e2e7fe
3KKK <ruikq@outlook.com>
2022-02-17 09:19:22 +0800

    resource parser example

---
+++ resourceParserExample.js
@@ -0,0 +1,22 @@
+/**
+ * 资源类型
+ * 0:config
+ * 1:nodes
+ * 2:rules
+ * 3:rewrites
+ * 4:scripts
+ * 5:plugin
+ */
+let resourceType = $resourceType;
+//资源内容
+let resource = $resource;
+//资源url
+let url = $resourceUrl;
+
+console.log(resourceType);
+console.log(url);
+
+if (resourceType == 0) {
+    //$done(解析后的资源字符串)
+    $done(resource);
+}
