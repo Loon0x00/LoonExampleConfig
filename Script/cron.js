353e059de1fc0c9d1fc844ae9bbb105ad8f39488
Loon <loon0x00@gmail.com>
2022-09-03 20:16:49 +0800

    cron

--- Script/cron.js
+++ Script/cron.js
@@ -1,5 +1,11 @@
 /*
+cron 语法
+* * * * * : 分 时 日 月 周
+* * * * * * :秒 分 时 日 月 周";
 
+脚本配置
+[script]
+cron "0 8 * * *" script-path=https://raw.githubusercontent.com/Loon0x00/LoonExampleConfig/master/Script/cron.js,tag = cronExample,enable=true
 */
 
 console.log('Start JS');
