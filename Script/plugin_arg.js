0964e605c46f10511f6e29814e8b2d8f126f4dae
Loon <loon0x00@gmail.com>
2024-08-09 22:03:07 +0800

    plugin example

---
+++ Script/plugin_arg.js
@@ -0,0 +1,11 @@
+/**
+ * 这是一个读取插件中用户配置参数的脚本，通过 $arguement.xxx 获取配置的相关参数
+ */
+
+let appName = $arguement.appName
+let category = $arguement.appCategory
+let isC = $arguement.isSupportChinese
+
+log($arguement)
+
+$done({});
