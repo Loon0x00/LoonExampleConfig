96a6fc742626a523f88949da043e82e10c86ab47
Loon <loon0x00@gmail.com>
2024-08-10 23:16:30 +0800

    icon & log

--- Script/plugin_arg.js
+++ Script/plugin_arg.js
@@ -2,10 +2,14 @@
  * 这是一个读取插件中用户配置参数的脚本，通过 $argument.xxx 获取配置的相关参数
  */
 
-let appName = $argument.appName
-let category = $argument.appCategory
-let isC = $argument.isSupportChinese
+console.log($argument);
 
-console.log($argument)
+let appName = $argument.appName;
+let category = $argument.appCategory;
+let isC = $argument.isSupportChinese;
+
+console.log("appName:" + appName);
+console.log("category:" + category);
+console.log("isSupportChinese:" + isC);
 
 $done({});
